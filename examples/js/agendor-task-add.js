a879a2f72d0c4d7ce5dd0f42ae742cff88e0170b
anaylton <victoranaylton@gmail.com>
2014-09-17 22:44:25 -0300

    Add example to agendor.task.add;

---
+++ examples/js/agendor-task-add.js
@@ -0,0 +1,59 @@
+/**
+ * This is an example of how to use Agendor wrapper to create a Person, a Deal related to
+ * this Person and a Task related to the Deal on Agendor.
+ *
+ *  - The Deal will be created with stage 'Contact' and status 'In Progress'
+ *
+ *
+ * First of all, set your Agendor API user token.
+ * - To see more about how to get a valid user token on Agendor API
+ * - go to: https://api.agendor.com.br
+ */
+agendor.setToken('PUT A VALID USER TOKEN HERE');
+
+$(document).on('submit','#contactForm', function(){
+    /**
+     * In this example, we're inserting a Person, a Deal and a Task.
+     * The Person will be inserted with values from the contact form.
+     * The Deal and the Task will be inserted with some hard-coded values.
+     *
+     * and the deal's description will be the value of the 'message' field from the contact form.
+     *
+     * - You can see all possible fields(required and optional) for person, deal and task here:
+     * - https://api.agendor.com.br/#people
+     * - https://api.agendor.com.br/#deals
+     * - https://api.agendor.com.br/#tasks
+     *
+     *
+     * Create a task object with all desired values...
+     */
+    var task = {
+        text: 'Contact this person urgently!',
+        deal:{
+            title: 'This person was inserted through the form in our web site.',
+            description: $('input[name="description"]').val(),
+            person: {
+                name: $('input[name="name"]').val(),
+                emails:[$('input[name="email"]').val()],
+                phones:[{
+                    number:$('input[name="phoneNumber"]').val(),
+                    type:'work'
+                }]
+            }
+        }
+    };
+
+    /**
+     * With everything set up, we can call agendor wrapper to insert our objects.
+     * The wrapper will call Agendor API and insert the person, the deal and the task.
+     */
+    agendor.task.add(task, function(result){
+        if(result.error){
+            //Something bad happened, 'result.error' is a message explaining what was wrong.
+        }else{
+            //The 'result' variable will be a task object with all fields inserted.
+        }
+    });
+
+    return false;
+});
